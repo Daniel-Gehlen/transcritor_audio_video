b2995ce974ce7a99da77e3391b037dcfa1963bbb
Daniel Gehlen <harmonia251251@gmail.com>
2025-01-10 09:25:08 -0300

    ffmpeg

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# Atualiza a lista de pacotes
+echo "Atualizando lista de pacotes..."
+apt-get update
+
 # Instala o FFmpeg
-apt-get update && apt-get install -y ffmpeg
+echo "Instalando FFmpeg..."
+apt-get install -y ffmpeg
+
+# Verifica se o FFmpeg foi instalado corretamente
+if command -v ffmpeg &> /dev/null; then
+    echo "FFmpeg instalado com sucesso!"
+else
+    echo "Erro ao instalar FFmpeg."
+    exit 1
+fi
